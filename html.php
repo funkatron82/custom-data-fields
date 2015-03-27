f5281225908e5441bf95391d8af1716e27362f07
Manny Fleurmond <funkatronic@gmail.com>
2015-03-26 20:31:01 -0500

    Update html.php

--- html.php
+++ html.php
@@ -11,7 +11,28 @@ if( !class_exists( 'CEDCDF_Field_Html' )){
     public $settings;
     
     function __construct( $settings ){
+      if( ! isset( $settings['name'] ) ){
+        return;
+      }
+      $this->name = esc_attr( $settings['name'] );
+      $this->value = $settings['value'] ? $this->esc_value( $settings['value'] ) : '';
       
+      $this->normalize( $settings );
     }
+  }
+  
+  function set_attributes( $defaults, $settings ){
+    $settings = (array) $settings;
+    foreach( $defaults as $key => $default ) {
+      $this->attributes[$key] = array_key_exists($key, $settings) ? $settings[$key] : $default;
+    }
+  }
+  
+  function normalize( $settings ){
+    $this->set_attributes( array(), $settings ); 
+  }
+  
+  function set_classes( $settings ){
+    
   }
 }
